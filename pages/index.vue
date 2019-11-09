6f93833d83cc3ee9136a37a9920ef25d3af706bc
nakano <kosuke@collab-it.net>
2019-11-09 18:52:09 +0900

    上にバーつけた

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,58 @@
 <template>
+  <div>
+    <Bar></Bar>
+    <v-container style="width: 100%; height: 100%;" class="grey lighten-5">
+      <v-row
+        class="mb-6"
+        no-gutters
+      >
+        <v-col
+          v-for="n in 4"
+          :key="n"
+        >
+          <v-card
+            class="pa-2"
+            tile
+            outlined
+          >
+            col
+          </v-card>
+        </v-col>
+      </v-row>
 
+      <v-row no-gutters>
+        <v-col
+          v-for="n in 2"
+          :key="n"
+          :cols="n === 1 ? 8 : 4"
+        >
+          <v-card
+            class="pa-2"
+            tile
+            outlined
+          >
+            col-{{ n === 1 ? 8 : 4 }}
+            <img class="sokujpg" src="@/static/soku.JPG">
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+  import Bar from "@/components/Bar";
 
-export default {
-}
+  export default {
+    components: {
+      Bar
+    }
+  }
 </script>
+
+<style>
+  .sokujpg {
+    height: 40%;
+    width: 40%;
+  }
+</style>
