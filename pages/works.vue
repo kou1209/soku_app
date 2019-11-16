e1d4a2007afde2d84b96333a4f6fe6c3c29768d1
nakano <kosuke@collab-it.net>
2019-11-16 15:15:36 +0900

    worksできた

--- pages/works.vue
+++ pages/works.vue
@@ -1,12 +1,24 @@
 <template>
   <v-container>
-    <h1>担当した業務</h1>
-
+    <h1 style="font-family: Monaco">担当した業務</h1>
+    <v-card style="margin-bottom: 20px;">
+      <v-card-text>
+        <h1 style="margin: 20px;">自社プロダクトの開発運用</h1>
+        <p style="margin-bottom: 10px;">期間：2019年7月〜現在</p>
+        <h2>RubyonRailsを用いた中〜大規模toC向けwebアプリの、新機能の開発、保守運用</h2>
+      </v-card-text>
+    </v-card>
+    <v-card style="margin-bottom: 20px;">
+      <v-card-text>
+        <h1 style="margin: 20px;">toB向けプロダクトの新機能開発</h1>
+        <p style="margin-bottom: 10px;">期間：2019年9月〜2019年10月</p>
+        <h2>Vue.jsを用いたtoB向けSPAの新機能開発。<br>VueRouter、Vuexを使用し、単一ファイルコンポーネントを用いる形での実装を行いました。<br>UIコンポーネントにはVuetifyを利用しました。</h2>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
-
 <script>
   export default {
-    layout: 'layout',
+    layout: 'layout'
   }
 </script>
